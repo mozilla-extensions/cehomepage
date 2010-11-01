f04e3e38d35957f7231cb13b489561cf5296bff6
pzhang <pzhang@mozilla.com>
2010-11-01 13:44:41 +0800

    1. migrated from cehomepage/extension
    Signed-off-by: pzhang <pzhang@mozilla.com>

---
+++ defaults/preferences/pref.js
@@ -0,0 +1,11 @@
+pref("extensions.cehomepage.autoSetHomepage",false);
+pref("extensions.cehomepage.keepsessions", 10);
+pref("extensions.cehomepage.homepage", "http://i.g-fox.cn/");
+pref("extensions.cehomepage.allowed_domains", ".g-fox.cn, localhost");
+pref("extensions.cehomepage.autostartup", true);
+//pref("browser.startup.homepage", "chrome://cehomepage/locale/cehomepage.properties");
+//pref("browser.startup.homepage_reset", "chrome://cehomepage/locale/cehomepage.properties");
+//pref("browser.startup.page", 1);
+pref("browser.warnOnQuit", true);
+pref("browser.warnOnRestart", true);
+pref("browser.tabs.warnOnClose", false);
