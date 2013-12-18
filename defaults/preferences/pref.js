cd2fddaf0a316805c03fa3819fb4222898538b03
Hector Zhao <bzhao@mozilla.com>
2013-12-18 08:57:38 +0800

    Bug 1102: use endsWith from MDN

--- defaults/preferences/pref.js
+++ defaults/preferences/pref.js
@@ -5,6 +5,7 @@ pref("extensions.cehomepage.homepage", "chrome://cehomepage/locale/cehomepage.pr
 pref("extensions.cehomepage.abouturl", "chrome://cehomepage/locale/cehomepage.properties");
 // have to be set as an complex value, or about:home will be get as homepage url.
 pref("browser.startup.homepage", "chrome://cehomepage/locale/cehomepage.properties");
+// ATTENTION: make sure allowed_domain always starts with a dot
 pref("extensions.cehomepage.allowed_domains", ".g-fox.cn, .firefoxchina.cn");
 pref("extensions.cehomepage.autostartup", true);
 //pref("browser.startup.homepage", "chrome://cehomepage/locale/cehomepage.properties");
