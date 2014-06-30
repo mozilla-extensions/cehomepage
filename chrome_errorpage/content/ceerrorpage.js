27d151fc8186f15ee768d9ccfa1427359ef3a156
Jinghe Hu <jihu@mozilla.com>
2014-06-30 17:57:54 +0800

    fix bug 1401: Redesign error page css style

--- chrome_errorpage/content/ceerrorpage.js
+++ chrome_errorpage/content/ceerrorpage.js
@@ -1,33 +1,70 @@
 (function() {
 	function addURLLink(event) {
 		var contentDoc=event.target;
+
 		var contentWin=contentDoc.defaultView;
 		if(contentDoc.documentURI.match(/^about:neterror/) && contentWin == contentWin.top){
-			var iframe = contentDoc.createElement('iframe');
-			iframe.width = '625px';
-			iframe.height = '0px';
-			iframe.style.border = 'none';
 			var errorPageContainer = contentDoc.getElementById('errorPageContainer');
-			errorPageContainer.appendChild(iframe);
-			iframe.src = 'http://i.firefoxchina.cn/newtab/net_error.html';
-			iframe.addEventListener('load', function(){
-				iframe.height = '330px';
-			}, false);
-			var timer = 0;
-			var interval = setInterval(function(){
-				if(timer < 150 && iframe.contentDocument) {
-					if(iframe.contentDocument.readyState == 'complete' || iframe.contentDocument.readyState == 'interactive') {
-						iframe.height = '330px';
+			var errorPageBody = contentDoc.body;
+
+			// Add "Go To Homepage" button
+			var goToHomePageBtn = contentDoc.createElement('button');
+			goToHomePageBtn.id = "goToHomePage";
+			var stringBundle = document.getElementById('ceerrorpage-strings');
+			goToHomePageBtn.textContent = stringBundle.getString("goToHomePage");
+			goToHomePageBtn.addEventListener("click", function () {
+			contentWin.location = "http://i.firefoxchina.cn/?from_err_btn";
+      });
+			var btnChildren = contentDoc.getElementsByTagName("button");
+			if (btnChildren) {
+				errorPageContainer.insertBefore(goToHomePageBtn, btnChildren[0]);
+			}
+			else {
+				errorPageContainer.appendChild(goToHomePageBtn);
+			}
+
+			// Reset the default error page css
+			var domWindowUtils = contentWin.QueryInterface(Ci.nsIInterfaceRequestor)
+			.getInterface(Ci.nsIDOMWindowUtils);
+			var cssUri = Services.io.newURI("chrome://ceerrorpage/skin/overlay.css", null, null);
+			domWindowUtils.loadSheet(cssUri, Ci.nsIDOMWindowUtils.AUTHOR_SHEET);
+
+			// Add site recomandation iframe to the default error page
+			if (window.navigator.onLine) {
+				var recomendIframe = contentDoc.createElement('iframe');
+				recomendIframe.id = "recomendIframe";
+				recomendIframe.height = '0px';
+				errorPageBody.appendChild(recomendIframe);
+				recomendIframe.src = 'http://newtab.firefoxchina.cn/error-tab-rec.html';
+				recomendIframe.addEventListener('load', function(){
+					if (recomendIframe.contentWindow.document.URL.match(/^about:neterror/)) {
+								errorPageBody.removeChild(recomendIframe);
+							}
+							else {
+								recomendIframe.height = '330px';
+							}
+				}, false);
+				var timer = 0;
+				var interval = setInterval(function(){
+					if(timer < 150 && recomendIframe.contentDocument) {
+						if(recomendIframe.contentDocument.readyState == 'complete' || recomendIframe.contentDocument.readyState == 'interactive') {
+							if (recomendIframe.contentWindow.document.URL.match(/^about:neterror/)) {
+								errorPageBody.removeChild(recomendIframe);
+							}
+							else {
+								recomendIframe.height = '330px';
+							}
+							timer = 0;
+							clearInterval(interval);
+						} else {
+							timer++;
+						}
+					} else {
 						timer = 0;
 						clearInterval(interval);
-					} else {
-						timer++;
 					}
-				} else {
-					timer = 0;
-					clearInterval(interval);
-				}
-			}, 200);
+				}, 200);
+			}
 		}
 	}
 
